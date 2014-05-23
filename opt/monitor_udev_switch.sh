b0142bf8992254ba8c17806ecff81aee82460c75
b3rserker <hubik.filip@gmail.com>
2014-05-23 00:50:24 +0000

    Udev script for external monitor hotplug(in/out)

---
+++ opt/monitor_udev_switch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export DISPLAY=:0.0
+
+xrandr | grep "VGA1 connected"
+if [[ $? == 0 ]]; then
+  echo 'connected'
+  # is connected
+  xrandr --output VGA1 --auto
+  xrandr --output LVDS1 --off
+else
+  # not connected
+  echo 'not'
+  xrandr --output VGA1 --off
+  xrandr --output LVDS1 --auto
+fi
